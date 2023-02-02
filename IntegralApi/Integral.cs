6fca49654a7f329b6c3dfeac88fc7e3a36ad7c30
Vladyslav Zhukov <epicmand.mi@gmail.com>
2023-02-03 01:32:10 +0200

    Add project files.

---
+++ IntegralApi/Integral.cs
@@ -0,0 +1,22 @@
+﻿using IntegralApi.Model;
+using Newtonsoft.Json;
+using System.Web;
+
+namespace IntegralApi
+{
+    static class Integral
+    {
+        public static async Task<string> Get(string equation)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync($"https://newton.vercel.app/api/v2/integrate/{HttpUtility.UrlEncode(equation)}"))
+                {
+
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<IntegralModel>(apiResponse)?.Result ?? "";
+                }
+            }
+        }
+    }
+}
---
+++ IntegralApi/Model/IntegralModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegralApi.Model
+{
+    public class IntegralModel
+    {
+        public string? Operation { get; set; }
+        public string? Expression { get; set; }
+        public string? Result { get; set; }
+    }
+}
